4d543b57c321bb95555d7c841b112e070abe675a
nksbks <121257584+nksbks@users.noreply.github.com>
2023-01-20 09:46:35 +0200

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,57 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # .
+        . . # . .
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # . .
+        . . # . .
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # .
+        . . # . .
+        # # # # #
+        `)
+    basic.pause(500)
+    if (input.lightLevel() > 128) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . # . .
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # . # #
+            `)
+    }
+    basic.pause(2000)
+})
